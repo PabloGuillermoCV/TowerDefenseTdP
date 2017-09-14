363ed94e3093f0e23b1eb4fe5a02ef34c65493e8
Pablo Guillermo <pgcb123@gmail.com>
2017-09-14 16:51:50 -0300

    correccion
    corrijo error importe raro

--- juego/src/GUI/juego.java
+++ juego/src/GUI/juego.java
@@ -17,7 +17,7 @@ import com.jgoodies.forms.layout.FormSpecs;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JTextField;
-import org.eclipse.wb.swing.FocusTraversalOnArray;
+
 import java.awt.Component;
 import javax.swing.JLayeredPane;
 import java.awt.Color;
@@ -120,7 +120,7 @@ public class juego {
 		Autores.setText("TdP 2017");
 		panel.add(Autores, "16, 18, center, bottom");
 		Autores.setColumns(10);
-		panel.setFocusTraversalPolicy(new FocusTraversalOnArray(new Component[]{jugar, Opciones, EXIT, Autores}));
+		
 		
 		JPanel panel_Mapa = new JPanel();
 		panel_Mapa.setBackground(Color.LIGHT_GRAY);
