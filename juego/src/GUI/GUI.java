65a656e79c80c2e7f12afabb9dd4fd53ddaef0aa
TomasUNS <31107142+TomasUNS@users.noreply.github.com>
2017-10-23 12:20:01 -0300

    Se ve la tienda
    Ahi le saqué la imagen de fondo a la tienda pero es algo que deberiamos ver porque no se ponen las cosas en el orden correcto en la tienda visual.

--- juego/src/GUI/GUI.java
+++ juego/src/GUI/GUI.java
@@ -14,7 +14,7 @@ public class GUI extends JFrame {
 	private static int widthS = 500;
 	private static int heightS = 400;
 	private static String direccionM = "src\\GUI\\Sprites Mapas\\Mapa1.png";
-	private static String direccionS = "src\\GUI\\Sprites Mapas\\FondoTienda.png";
+	private static String direccionS = "src\\GUI\\Sprites Mapas\\FondoTiendaOff.png";
 	private MapaVisual mapa;
 	private TiendaVisual shop;
 	private Nivel nivel;
