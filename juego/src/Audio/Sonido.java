45d1ef501408488a2cd8ccb30876397689f98e60
Pablo Guillermo <pgcb123@gmail.com>
2017-11-05 11:43:28 -0300

    mudanza música
    mudé la reproducción de la música a la clase Sonido, despues tenemos que sacarle las responsabilidades a ContadorTiempo

--- juego/src/Audio/Sonido.java
+++ juego/src/Audio/Sonido.java
@@ -2,62 +2,106 @@ package Audio;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Random;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
 import javax.swing.JFrame;
 
-@SuppressWarnings("serial")
-public class Sonido extends JFrame {
+public class Sonido {
 	
-    JButton[] boton1 = new JButton[5];
-    Random x = new Random();
-    ImageIcon[] cara = new ImageIcon[5];
-    
-    public Sonido () {
-    	/*super("Pulse el boton");
-    	for (int i = 0; i < 5; i++) {
-    		cara[i] = new ImageIcon("src/GUI/Sprites Botones/cara" + (i+1) + ".png");
-    		boton1[i] = new JButton();
-    		boton1[i].setLayout(new GridLayout());
-    		boton1[i].setIcon(cara[i]);
-    		//boton1[i].addActionListener(this);
-    		add(boton1[i]);
-    	}
-        setSize(400,400);
-        setLocationRelativeTo(null);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLayout(new FlowLayout());
-        boton1 [0].setText ("" + 200);
-        boton1 [1].setText ("" + 300);
-        boton1 [2].setText ("" + 500);
-        boton1 [3].setText ("" + 500);
-        boton1 [4].setText ("" + 650);
-        
-        setVisible(true);*/
-    }
-    
-    /*public static void main(String[] Dar10){
-        new Sonido();
-    }*/
-    
-    /**
-	 * metodo que reproduce sonidos (para botones en particular, tengo entendido que para lo que seria BGM tenemos que hacer un Thread)
-	 * @param nombreSonido direccion al archivo de sonido (DEBE SER UN .wav, NO funciona con otros formatos)
+	private AudioInputStream audioInputStream;
+	private Clip clip;
+	private static Sonido Instancia;
+	
+	public static Sonido getInstancia() {
+		if(Instancia == null) {
+			Instancia = new Sonido();
+		}
+		
+		return Instancia;
+	}
+	
+	/**
+	 * método que inicia la música seteada PREVIAMENTE por el método setAudio(File)
 	 */
-    public void ReproducirSonido(String nombreSonido){
-        try {
-    	    AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File(nombreSonido).getAbsoluteFile());
-       	    Clip clip = AudioSystem.getClip();
-       	    clip.open(audioInputStream);
-       	    clip.start();
-        } catch(UnsupportedAudioFileException | IOException | LineUnavailableException ex) {
-    	    System.out.println("Error al reproducir el sonido.");
-        }
-    }
+	public void Activar() {
+		clip.start();
+		clip.loop(Clip.LOOP_CONTINUOUSLY);
+	}
+	
+	/**
+	 * método que detiene la música seteada PREVIAMENTE por el método setAudio(File)
+	 */
+	public void Desactivar() {
+		clip.stop();
+	}
+	
+	/**
+	 * método que reproduce la música de victoria
+	 */
+	public void Victoria() {
+		audioInputStream = null;
+		try {
+			audioInputStream = AudioSystem.getAudioInputStream (new File("src\\Audio\\Audio.Sonidos\\Victoria.WAV").getAbsoluteFile());
+		}
+		catch (UnsupportedAudioFileException | IOException e) {
+			e.printStackTrace ();
+		}
+		try {
+			clip = AudioSystem.getClip ();
+			if (audioInputStream != null) {
+				clip.open (audioInputStream);
+				clip.start ();
+			}
+		}
+				catch (LineUnavailableException|IOException e) {
+					e.printStackTrace ();
+				}
+	}
+	
+	/**
+	 * método que reproduce la música de Derrota
+	 */
+	public void Derrota() {
+		audioInputStream = null;
+		JFrame frame = new JFrame ();
+		frame.setBounds (100, 100, 450, 300); //VER ESTO
+		frame.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
+		try {
+			audioInputStream = AudioSystem.getAudioInputStream (new File("src\\Audio\\Audio.Sonidos\\Derrota.WAV").getAbsoluteFile());
+		}
+		catch (UnsupportedAudioFileException | IOException e) {
+			e.printStackTrace ();
+		}
+		try {
+			clip = AudioSystem.getClip ();
+			if (audioInputStream != null) {
+				clip.open (audioInputStream);
+				clip.start ();
+			}
+		}
+		catch (LineUnavailableException|IOException e) {
+			e.printStackTrace ();
+		}
+	}
+	
+	/**
+	 * método que setea la canción a reproducir
+	 * @param cancion dirección de la canción a reproducir
+	 */
+	public void setAudio(File cancion){
+		try {
+			audioInputStream = AudioSystem.getAudioInputStream(cancion);
+			clip.open(audioInputStream); //cuando seteo el audio lo abro, con esto manejamos el audio de los niveles
+			//despues se llama a Sonido.activar();
+		} catch (UnsupportedAudioFileException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (LineUnavailableException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}	
 }
