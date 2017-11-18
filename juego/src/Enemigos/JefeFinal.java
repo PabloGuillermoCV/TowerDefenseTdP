13e826e0ff0daf6eb36ab40aae0a70449d6fd574
Pablo Guillermo <pgcb123@gmail.com>
2017-11-17 21:37:36 -0300

    FInal balances
    Listo los balanceos finales

--- juego/src/Enemigos/JefeFinal.java
+++ juego/src/Enemigos/JefeFinal.java
@@ -10,17 +10,17 @@ public class JefeFinal extends Enemigo {
 	Nombre = "JefeFinal"
 	JLabel = JefeFinal.gif
 	Posicion = pos
-	Vida = 750
+	Vida = 600
 	Alcance = 4
 	Ataque = 85
-	Defensa = 350
+	Defensa = 250
 	VelocidadMov = 4
 	PowerUp = False
 	Puntaje = 10000
 	*/
 	
 	public JefeFinal (Posicion pos) {
-		super ("JefeFinal", pos, 750, 4, 85, 350, 4, false, 1000, 10000);
+		super ("JefeFinal", pos, 600, 4, 85, 250, 4, false, 1000, 10000);
 		grafico = new EntidadGraficaEnemigo ("src\\Enemigos\\Sprites Enemigos\\JefeFinal.gif",pos,this);
 	}
 }
